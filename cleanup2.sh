5f8c31840ab6f1e0f895e3799e71e9aafefeae24
Sita Liu <chsliu@gmail.com>
2015-03-05 17:58:18 +0800

    minor changes

--- cleanup2.sh
+++ cleanup2.sh
@@ -3,4 +3,4 @@
 sudo apt-get -y purge $( dpkg --list | grep -P -o "linux-image-\d\S+" | grep -v $(uname -r | grep -P -o ".+\d") )
 sudo apt-get -y purge $( dpkg --list | grep -P -o "linux-headers-\d\S+" | grep -v $(uname -r | grep -P -o ".+\d") )
 
-sh cleanup.sh
+sh ~/script/cleanup.sh
