2d0302af70f4ebdff5bdcb7ccd987a03e7573cdb
Sita Liu <chsliu@gmail.com>
2015-06-17 08:03:02 +0800

    Automated commit at 06/17/15 08:03:01 on

--- gitconf.sh
+++ gitconf.sh
@@ -2,4 +2,6 @@ git config --global user.email "chsliu@gmail.com"
 
 git config --global user.name "Sita Liu"
 
+git config --global credential.helper store
+
 git config --global push.default matching
