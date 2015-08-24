71d10d57a711900b2b713d2e618e6ce8a2a01f60
Sita Liu <chsliu@gmail.com>
2015-08-24 13:57:16 -0700

    Automated commit at 08/24/15 13:57:16 on vm1404

--- ydown.mp3only.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-youtube-dl -c -i -o 'mp3/%(title)s (%(height)sp).%(ext)s' -k -x --audio-format mp3 --audio-quality 176 --prefer-ffmpeg $1
--- ydown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-youtube-dl -c -i -f 'bestvideo[height<=?480]+bestaudio/best' -o '%(title)s (%(height)sp).%(ext)s' --prefer-ffmpeg $1
-
-#sudo youtube-dl -v -c -i -f 'bestvideo[height<=?480]+bestaudio/best' --prefer-ffmpeg $1
