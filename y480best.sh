4ee022e720cc5ae28872bb740754443dcd9d63eb
Sita Liu <chsliu@gmail.com>
2015-08-31 20:03:21 -0700

    Automated commit at 08/31/15 20:03:21 on vm1404

--- y480best.sh
+++ y480best.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-youtube-dl -c -i -f 'bestvideo[height=480]+bestaudio/best' -o 'video/%(title)s (%(height)sp).%(ext)s' --prefer-ffmpeg $1
+youtube-dl -c -i -f 'bestvideo[height=480]+bestaudio/best' -o 'video/%(title)s (%(height)sp).%(ext)s' --prefer-ffmpeg $*
