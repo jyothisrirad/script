649ba33bdb42ae4e65e105c052ea98e592e65df6
Sita Liu <chsliu@gmail.com>
2015-09-01 11:05:29 +0800

    Automated commit at 2015/09/01 週二  上午 11:05   on W1

--- yworst480.sh
+++ yworst480.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-youtube-dl -c -i -f 'mp4[height=480]+worst/best' -o 'video/%(title)s (%(height)sp).%(ext)s' --prefer-ffmpeg $1
+youtube-dl -c -i -f 'mp4[height=480]+worst/best' -o 'video/%(title)s (%(height)sp).%(ext)s' --prefer-ffmpeg $*
