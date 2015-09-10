8baaca8383ee9fc3e51e8c45e52e130f6c218ad2
Sita Liu <chsliu@gmail.com>
2015-09-10 18:52:48 +0800

    Automated commit at 09/10/15 18:52:48 on vm1404

--- backuphere.sh
+++ backuphere.sh
@@ -1,1 +1,11 @@
-rsync -avz --progress --chmod=a=rw,Da+x --fake-super --delete . rsync://sitahome.no-ip.org/NetBackup/sita/$(hostname)$(pwd)
+echo Backup $(whoami)/$(hostname)$(pwd) ...
+
+mkdir /tmp/empty
+
+rsync -a /tmp/empty rsync://sitahome.no-ip.org/NetBackup/$(whoami)
+
+rsync -a /tmp/empty rsync://sitahome.no-ip.org/NetBackup/$(whoami)/$(hostname)
+
+rm -r /tmp/empty
+
+rsync -avz --progress --chmod=a=rw,Da+x --fake-super --delete -R $(pwd) rsync://sitahome.no-ip.org/NetBackup/$(whoami)/$(hostname)
