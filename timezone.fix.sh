40a366d0e3dd2bec8019505cbd65d298c2f08031
Sita Liu <chsliu@gmail.com>
2015-09-13 23:21:00 +0800

    Automated commit at 09/13/15 23:21:00 on debian

--- timezone.fix.sh
+++ timezone.fix.sh
@@ -2,7 +2,7 @@ sudo dpkg-reconfigure tzdata
 
 sudo cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 
-sudo apt-get install ntpdate
+sudo apt-get install -y ntpdate
 
 sudo ntpdate time.stdtime.gov.tw
 sudo ntpdate time.stdtime.gov.tw
