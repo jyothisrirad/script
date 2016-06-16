a8cc24b2896ba8aef4789edc482376cfdc158e4f
Sita Liu <chsliu@gmail.com>
2016-06-16 12:28:32 +0800

    Automated commit at 06/16/16 12:28:32 on vm1404

---
+++ gateway.reboot.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/expect -f
+
+spawn ssh cht@192.168.1.250
+expect {
+	"password: " {
+		send_user "<Enter Password ...>\n"
+		send "chtnvdsl\r"
+	} incorrect {
+		send_user "<invalid password or account>\n"
+		exit
+	} timeout {
+		send_user "<Timeout on Password>\n"
+		exit
+	} eof {
+		send_user "<Connection Failed on Password>\n"
+		exit
+	}
+}
+
+expect {
+	"21." {
+		send_user "<Enter Selection 20>\n"
+		send "20\r"
+	} timeout {
+		send_user "<Timeout on Option 1>\n"
+		exit
+	} eof {
+		send_user "<Connection Failed on Option 1>\n"
+		exit
+	}
+}
+
+expect {
+	"07." {
+		send_user "<Enter Selection 1>\n"
+		send "1\r"
+	} timeout {
+		send_user "<Timeout on Option 2>\n"
+		exit
+	} eof {
+		send_user "<Connection Failed on Option 2>\n"
+		exit
+	}
+}
+
+expect {
+	"Confirm Reboot" {
+		send_user "<Enter Selection y>\n"
+		send "y\r"
+	} timeout {
+		send_user "<Timeout on Option 3>\n"
+		exit
+	} eof {
+		send_user "<Connection Failed on Option 3>\n"
+		exit
+	}
+}
+
+#sleep 10
+expect {
+	"cleanup" {
+		send_user "<Complete>\n"
+		exit
+	} timeout {
+		send_user "<Timeout on Final>\n"
+		exit
+	} eof {
+		send_user "<Connection Failed on Final>\n"
+		exit
+	}
+}
