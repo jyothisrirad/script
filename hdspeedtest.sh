c45f95818d6ca3572db7df02b2888e8ffd4af5da
Sita Liu <chsliu@gmail.com>
2016-11-06 07:37:02 +0800

    Automated commit at 11/06/16 07:37:02 on pve

--- hdspeedtest.sh
+++ hdspeedtest.sh
@@ -1,23 +1,55 @@
 #!/bin/bash
 
-echo 3 | sudo tee /proc/sys/vm/drop_caches
+loop=150000
+blocksize=1M
 
-echo Testing IOPS with 150GB, 150K of 1M writes
+isroot() {                                                     
+  if [ $(whoami) = "root" ]; then return 0; else return 1; fi  
+}                                                              
+
+diskflush() {
+sync
+
+if isroot; then 
+  echo 3 > /proc/sys/vm/drop_caches
+else 
+  echo 3 | sudo tee /proc/sys/vm/drop_caches
+fi
+
+}
+
+benchmark() {
+diskflush
+
 testfile=zerofile.tmp
-result=$(dd if=/dev/zero of=$testfile bs=1M count=10 2>&1)
+result=$(dd if=/dev/zero of=$testfile bs=$blocksize count=$loop 2>&1)
 rm $testfile
 
 
+result=$(echo $result | sed "s/dd: error writing $(echo -ne '\u2018')zerofile.tmp$(echo -ne '\u2019'): No space left on device//")
 speed=$(echo $result | awk '{print $14}') 
 unit=$(echo $result | awk '{print $15}') 
 count=$(echo $result | awk '{print $1}') 
 totalsec=$(echo $result | awk '{print $12}') 
 iops=$(awk "BEGIN {print ($count)/$totalsec}")
 
-echo =====
-echo Computer: $(hostname)
-echo =====
+
 echo $result
-echo =====
+echo ------------------------------------
 echo Write: $speed $unit
 echo IOPS: $iops
+}
+
+echo Flushing Disk Cache
+diskflush
+
+echo Testing IOPS with 150GB, $loop of $blocksize writes
+echo ====================================
+echo Computer: $(hostname)
+echo ====================================
+benchmark
+echo ====================================
+benchmark
+echo ====================================
+benchmark
+
