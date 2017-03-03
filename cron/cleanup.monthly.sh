8535e06b8c4e3ec1df5d0233745695566a8a36f9
Sita Liu <chsliu@gmail.com>
2017-03-03 18:24:37 +0800

    Automated commit at 03/03/17 18:24:37 on pve5

--- cron/cleanup.monthly.sh
+++ cron/cleanup.monthly.sh
@@ -40,7 +40,7 @@ delfrom_crontab() {
 
 #=================================
 main() {
-	echo $cron_period
+	rm -rf /tank/Shares/Download/DEL
 }
 
 #=================================
@@ -48,7 +48,7 @@ case "$1" in
   install)
 	addto_crontab ""
 	addto_crontab "$cron_comment"
-	addto_crontab "$cron_min $cron_hour * * * /usr/bin/batch < $(readlink -e $0)"
+	addto_crontab "$cron_min $cron_hour 1 * * /usr/bin/batch < $(readlink -e $0)"
 	;;
   uninstall)
 	delfrom_crontab "$cron_comment"
