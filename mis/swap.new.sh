38de1564debe40236b95c8755f3f51f15248e4f5
Sita Liu <chsliu@gmail.com>
2017-04-15 18:41:00 +0800

    Automated commit at 04/15/17 18:41:00 on vb1704

--- mis/swap.new.sh
+++ mis/swap.new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-swapsize=$(grep MemTotal /proc/meminfo | awk '{print $2 }' | xargs -I {} echo "sqrt({}/1024^2)" | bc)
+swapsize=$(grep MemTotal /proc/meminfo | awk '{print $2 }' | xargs -I {} echo "sqrt({}/1024^2)" | bc)G
 
 addto_fstab() {
 	fsfile=/etc/fstab
@@ -14,32 +14,15 @@ delfrom_fstab() {
 	sudo sed -i "/$1/d" /etc/fstab
 }
 
-echo sudo fallocate -l $swapsizeG /swapfile
-exit 
 sudo swapon --show
 sudo swapoff -a
 #delete swap from fstab
 delfrom_fstab swap
-echo sudo fallocate -l $swapsizeG /swapfile
-exit 
+echo sudo fallocate -l $swapsize /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 # echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 addto_fstab /swapfile none swap sw 0 0
 sudo swapon --show
-
-#server
-echo swappiness
-cat /proc/sys/vm/swappiness
-echo vfs_cache_pressure
-cat /proc/sys/vm/vfs_cache_pressure
-echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
-echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
-
-
-#desktop
-cat /proc/sys/vm/swappiness
-cat /proc/sys/vm/vfs_cache_pressure
-echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
 
