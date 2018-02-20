3d43d4148f5d01551656dfb341f8f3eb5ebb0218
Sita Liu <chsliu@gmail.com>
2018-02-20 16:10:21 +0800

    Automated commit at 02/20/18 16:10:21 on lxcmc204

--- minecraft/azure/init.sh
+++ minecraft/azure/init.sh
@@ -6,8 +6,8 @@ sudo chown sita:sita /mnt/backup
 
 sudo apt install -y zip
 
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 sudo apt update
 sudo apt install -y oracle-java8-installer
