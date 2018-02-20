9b9ad43263f240a29c45a59981e180db8efe62ff
Sita Liu <chsliu@gmail.com>
2018-02-20 16:03:49 +0800

    Automated commit at 02/20/18 16:03:49 on lxcmc204

--- minecraft/azure/init.sh
+++ minecraft/azure/init.sh
@@ -6,6 +6,7 @@ sudo chown sita:sita /mnt/backup
 
 sudo apt install -y zip
 
+sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt install -y oracle-java8-installer
