c1048dd4f1c85511ba904df3d742eff89dca5f47
Sita Liu <chsliu@gmail.com>
2018-02-26 14:33:25 +0800

    Automated commit at 02/26/18 14:33:25 on 65-bungeeCord-HK

--- minecraft/azure/init.bungee.sh
+++ minecraft/azure/init.bungee.sh
@@ -1,8 +1,13 @@
-65
-mcrestore
+#!/bin/bash
 
-sudo ln -s /mnt/runtimes/65-bungeeCord/logrotate /etc/logrotate.d/bungeecord
-sudo chown root /mnt/runtimes/65-bungeeCord/logrotate
+. /home/sita/script/minecraft/alias.minecraft
+
+s65
+mcserver restore
+
+sudo rm /etc/logrotate.d/bungeecord
+sudo ln -s /mnt/runtimes/65-bungeeCord-azure/logrotate /etc/logrotate.d/bungeecord
+sudo chown root /mnt/runtimes/65-bungeeCord-azure/logrotate
 sudo logrotate -d /etc/logrotate.d/bungeecord
 
 ~/script/init.d/inst.as.sh ~/script/init.d/bungee.d bungeestart
