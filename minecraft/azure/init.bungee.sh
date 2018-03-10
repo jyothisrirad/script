72ae6e5a9f6d0d8d69248d3bb1fcb4420663df02
Sita Liu <chsliu@gmail.com>
2018-03-10 15:19:56 +0800

    Automated commit at 03/10/18 15:19:56 on lxcmc204

--- minecraft/azure/init.bungee.sh
+++ minecraft/azure/init.bungee.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 ### 
-. /home/sita/script/minecraft/alias.minecraft
-
-### 
 addto_root_crontab() {
 	su - root -c "(crontab -l; echo \"$*\") | crontab -"
 }
+
+### 
+addto_root_crontab "0 23 * * * /sbin/poweroff"
+~/script/init.d/inst.as.sh ~/script/init.d/bungee.d bungeestart
+
+### 
+. /home/sita/script/minecraft/alias.minecraft
 
 ### 
 s65
@@ -16,7 +20,3 @@ sudo rm /etc/logrotate.d/bungeecord
 sudo ln -s /mnt/runtimes/65-bungeeCord-azure/logrotate /etc/logrotate.d/bungeecord
 sudo chown root /mnt/runtimes/65-bungeeCord-azure/logrotate
 sudo logrotate -d /etc/logrotate.d/bungeecord
-
-~/script/init.d/inst.as.sh ~/script/init.d/bungee.d bungeestart
-
-addto_root_crontab "0 23 * * * /sbin/poweroff"
