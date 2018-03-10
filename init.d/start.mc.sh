7335e459ee05c4bda83e58825d88b2bffee53409
Sita Liu <chsliu@gmail.com>
2018-03-11 01:41:56 +0800

    Automated commit at 03/11/18 01:41:56 on lxcmc206

--- init.d/start.mc.sh
+++ init.d/start.mc.sh
@@ -6,6 +6,4 @@
 s74 && mcserver start
 
 ###
-HOST=uhc
-/home/sita/script/mis/gcloud.dns.update.sh A creeper-tw $HOST creeper.tw 5min
-# /home/sita/script/mis/gcloud.dns.update.sh CNAME creeper-tw mc creeper.tw $HOST.creeper.tw. 1min
+/home/sita/script/minecraft/gcloud/uhc
