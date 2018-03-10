cd09c6df3e3fdcfd03a64b4680335d2e5ecaaf52
Sita Liu <chsliu@gmail.com>
2018-03-10 17:55:34 +0800

    Automated commit at 03/10/18 17:55:34 on lxcmc204

---
+++ mis/gcloud.dns.update.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+ttlify() {
+  local i
+  for i in "$@"; do
+    [[ "${i}" =~ ^([0-9]+)([a-z]*)$ ]] || continue
+    local num="${BASH_REMATCH[1]}"
+    local unit="${BASH_REMATCH[2]}"
+    case "${unit}" in
+                     weeks|week|wee|we|w) unit=''; num=$[num*60*60*24*7];;
+                           days|day|da|d) unit=''; num=$[num*60*60*24];;
+                     hours|hour|hou|ho|h) unit=''; num=$[num*60*60];;
+      minutes|minute|minut|minu|min|mi|m) unit=''; num=$[num*60];;
+      seconds|second|secon|seco|sec|se|s) unit=''; num=$[num];;
+    esac
+    echo "${num}${unit}"
+  done
+}
+
+dns_start() {
+  gcloud dns record-sets transaction start    -z "${ZONENAME}"
+}
+
+dns_info() {
+  gcloud dns record-sets transaction describe -z "${ZONENAME}"
+}
+
+dns_abort() {
+  gcloud dns record-sets transaction abort    -z "${ZONENAME}"
+}
+
+dns_commit() {
+  gcloud dns record-sets transaction execute  -z "${ZONENAME}"
+}
+
+dns_add() {
+  if [[ -n "$1" && "$1" != '@' ]]; then
+    local -r name="$1.${ZONE}."
+  else
+    local -r name="${ZONE}."
+  fi
+  local -r ttl="$(ttlify "$2")"
+  local -r type="$3"
+  shift 3
+  gcloud dns record-sets transaction add      -z "${ZONENAME}" --name "${name}" --ttl "${ttl}" --type "${type}" "$@"
+}
+
+dns_del() {
+  if [[ -n "$1" && "$1" != '@' ]]; then
+    local -r name="$1.${ZONE}."
+  else
+    local -r name="${ZONE}."
+  fi
+  local -r ttl="$(ttlify "$2")"
+  local -r type="$3"
+  shift 3
+  gcloud dns record-sets transaction remove   -z "${ZONENAME}" --name "${name}" --ttl "${ttl}" --type "${type}" "$@"
+}
+
+lookup_dns_ip() {
+  host "$1" | sed -rn 's@^.* has address @@p'
+}
+
+my_ip() {
+  # ip -4 addr show dev eth0 | sed -rn 's@^    inet ([0-9.]+).*@\1@p'
+  curl https://api.ipify.org
+}
+
+doit() {
+  ZONENAME=$1
+  ZONE=$2
+  HOST=$3
+  TTL=$4
+  dns_start
+  dns_del ${HOST} ${TTL} A `lookup_dns_ip "${HOST}.${ZONE}."`
+  dns_add ${HOST} ${TTL} A `my_ip`
+  dns_commit
+}
+
+# doit foo-bar-com foo.bar.com my-vm 5min
+doit $1 $2 $3 $4
