9f0cc6b626843df2c5f56e91513a5f38418fb3da
Sita Liu <chsliu@gmail.com>
2018-03-18 13:52:15 +0800

    Automated commit at 03/18/18 13:52:15 on lxcmc204

--- init.d/start.mc.sh
+++ init.d/start.mc.sh
@@ -13,12 +13,19 @@ stop() {
   s74 && mcserver halt
 }
 
+checklag() {
+  s74 && logs | grep.lag
+}
+
 case "$1" in
   start)
     start
     ;;
   stop)
     stop
+    ;;
+  lag)
+    checklag
     ;;
   *)
     start
