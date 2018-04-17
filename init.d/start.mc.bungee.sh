d065ca10c58891f024941fb3478a11dd838709ad
Sita Liu <chsliu@gmail.com>
2018-04-17 17:56:33 +0800

    Automated commit at 04/17/18 17:56:33 on lxcmc206

--- init.d/start.mc.bungee.sh
+++ init.d/start.mc.bungee.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 . /home/sita/script/minecraft/alias.minecraft
+
+mcdns=tw1
+mchub=tp1.creeper.tw
+mchubport=20468
+hostnames=($mcdns)
 servers=(s32 s65)
 
 mcstart() {
@@ -13,7 +18,7 @@ mcstart() {
 start() {
     cd ~/script && ./gitsync.sh
   
-	/home/sita/script/minecraft/gcloud/tw1
+	/home/sita/script/minecraft/gcloud/$mcdns
     
     # mcstart
 }
@@ -39,11 +44,49 @@ checkclag() {
 	done
 }
 
+checkip() {
+    # echo Checking $1
+    thisip=$(curl -s https://api.ipify.org)
+    thisdns=$(dig $1.creeper.tw | grep IN | grep -v ";" | awk '{ printf ("%s\n", $5) }')
+    # echo $myip $mydns
+    [ $thisip == $thisdns ] && return 0 || return 1
+}
+
+waiting() {
+    waitmax=${1:-10}
+    printf "Waiting"
+    for ((i=1;i<=${waitmax};i++)); do
+        printf "."
+        sleep 1
+    done
+    printf "\n"
+}
+
+testconnect() {
+    addr=$1
+    port=$2
+    timeout 1 bash -c "cat < /dev/null > /dev/tcp/$addr/$port"
+    return $?
+}
+
 case "$1" in
 	start)
 		start
 		;;
 	mcstart)
+        while [ 1 ]; do
+            if checkip $mcdns; then
+                echo === IP and DNS matched
+                if testconnect $mchub $mchubport; then
+                    echo === Hub connected
+                    echo === Starting Minecraft Server
+                    break
+                fi
+            else
+                echo IP and DNS not matched
+            fi
+            waiting 10
+        done
 		mcstart
 		;;
 	stop)
