8587333499455f56a49e28c900474306c5ced873
Sita Liu <chsliu@gmail.com>
2018-04-19 22:20:49 +0800

    Automated commit at 04/19/18 22:20:49 on lxcmc204

--- rsync/commit/config.sh
+++ rsync/commit/config.sh
@@ -1,9 +1,5 @@
 #/bin/bash
 
-#============================================
-. /home/sita/script/minecraft/include/java_args.sh
-
-#============================================
 TODAY=$(date +"%Y-%m-%d")
 MONTH=$(date +"%Y-%m")
 
@@ -11,15 +7,3 @@ MONTH=$(date +"%Y-%m")
 export backup_remote_root=recycle/$MONTH/$TODAY/rsync
 export backup_local_root=$backup_remote_root
 export rsync_root=sita.ddns.net/NetBackup/rsync
-
-#============================================
-USERNAME="sita"
-ZIPDIR=/mnt/backup
-BACKUPPATH="/mnt/backup"
-JARFILE=$(basename $(find . -maxdepth 1 -name '*.jar' | head -n 1))
-LOG="$MCPATH/proxy.log.0"
-OPTMEM="-Xmx1G -Dfile.encoding=UTF-8"
-JAVAARGS="$OPTMEM $OPTGC $OPTLOG"
-
-NEWJAR=$BACKUPPATH/server/$JARFILE
-[ -f $NEWJAR ] || rsync -avzq $NEWJAR .
