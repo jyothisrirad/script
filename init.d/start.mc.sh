b20cc595f8ef2164823a2f111b1033e249d52a4e
Sita Liu <chsliu@gmail.com>
2018-04-22 04:04:30 +0800

    Automated commit at 04/22/18 04:04:30 on lxcmc206

--- init.d/start.mc.sh
+++ init.d/start.mc.sh
@@ -18,6 +18,7 @@ as_user() {
 mcstart() {
 	for srv in ${servers[*]}
 	do
+		$srv && [ -f update.sh ] && echo -e "${GREEN}=== Running $mcver/update.sh${NC}" && ./update.sh
 		$srv && mcserver start
 	done
 }
