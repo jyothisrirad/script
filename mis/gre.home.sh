580c2e215ceb80ef5b69721eb14454b1a9ad3caa
Sita Liu <chsliu@gmail.com>
2018-07-05 18:18:30 +0800

    Automated commit at 07/05/18 18:18:30 on lxcmc206

--- mis/gre.home.sh
+++ mis/gre.home.sh
@@ -8,20 +8,22 @@ findmyip() {
 	curl -s http://me.gandi.net
 }
 
+DEV="gre1"
+
 case "$1" in
   start)
-	sudo ip link set gre0 down
-	sudo ip tunnel del gre0
-	# sudo ip tunnel add gre0 mode gre remote 104.196.251.199 local 118.168.234.13 ttl 255
-	sudo ip tunnel add gre0 mode gre remote $(lookup_dns_ip www.creeper.tw) local $(findmyip) ttl 255
-	sudo ip link set gre0 up
-	sudo ip addr add 10.10.10.2/24 dev gre0
+	sudo ip link set $DEV down
+	sudo ip tunnel del $DEV
+	# sudo ip tunnel add gre1 mode gre remote 104.196.251.199 local 118.168.234.13 ttl 255
+	sudo ip tunnel add $DEV mode gre remote $(lookup_dns_ip www.creeper.tw) local $(findmyip) ttl 255
+	sudo ip link set $DEV up
+	sudo ip addr add 10.10.10.2/24 dev $DEV
 	ip route show
     exit
     ;;
   stop)
-	sudo ip link set gre0 down
-	sudo ip tunnel del gre0
+	sudo ip link set $DEV down
+	sudo ip tunnel del $DEV
 	ip route show
     exit
     ;;
