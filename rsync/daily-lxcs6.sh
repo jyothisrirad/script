105a9a06189038c1deede3bd331d08e3f9f8006b
Sita Liu <chsliu@gmail.com>
2018-09-08 20:29:28 +0000

    Automated commit at 09/08/18 20:29:28 on lxcs6

--- rsync/daily-lxcs6.sh
+++ rsync/daily-lxcs6.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+exec > >(tee -a -i /tmp/log/$(basename $0).log)
+exec 2>&1
+
+echo 
+echo ===================
+echo $(date +%F_%T)
+echo ===================
+
 . /home/admin/script/rsync/backup.sh /share/
 
--- rsync/host.sh
+++ rsync/host.sh
@@ -1,5 +1,7 @@
 #HOST=sitahome.no-ip.org
-HOST=sitahome.ddns.net
+#HOST=sitahome.ddns.net
+#HOST=sita.ddns.net
+HOST=home.changen.com.tw
 HOSTPATH=NetBackup
 
 export HOST HOSTPATH
